4581bd1e1a87c16fdf7ee43184ddba73acb40467
KristianWB <42993999+KristianWB@users.noreply.github.com>
2018-10-01 15:51:55 +0200

    Initial commit

---
+++ src/DisplayFiveMessages.java
@@ -0,0 +1,11 @@
+public class DisplayFiveMessages {
+    public static void main(String[] args)    {
+
+        // Coding five messages i row
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+    }
+}
